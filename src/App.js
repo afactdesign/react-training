69dc548e99774234e0831a15d7b48b3f3bc3fa06
afactdesign <aoi@afactdesign.com>
2022-07-06 21:54:43 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Article from './components/Article';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Article
+        title={'Sample Title'}
+        content={'Content shows here vaa'}
+        />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import {Content, Title} from "./index";
+//import Content from "./Content";
+//import Title from "./Title";
+
+const Article = (props) => {
+    return(
+        <div>
+            <Title title={props.title} />
+            <Content content={props.content} />
+        </div>
+    );
+};
+
+export default Article;
